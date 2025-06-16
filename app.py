55d01b92a2b1cff93ad2f85cf729b7df7009a11b
Nizar <Nizar.MAHMOUDI@esprit.tn>
2025-06-16 22:36:16 +0100

    Add Dockerfile

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask import (
 )
 import numpy as np
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -62,4 +63,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
